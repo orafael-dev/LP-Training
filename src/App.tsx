47f550ec4f88434891fbca989c0dd081875cd9c6
Rafael Almeida <almeidarp.dev@gmail.com>
2024-08-13 11:19:44 -0300

    feat: Navbar component and hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,38 @@
-import Navbar from "@/scenes/Navbar"
-import { useState } from "react"
+import Navbar from "@/scenes/Navbar";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "./shared/types";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState<string>('home')
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
-    <div 
-    className="
+    <div
+      className="
     app
     bg-gray-20
-    ">
+    "
+    >
       <Navbar
-      selectedPage={selectedPage}
-      setSelectedPage={setSelectedPage}
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
       />
+      <div className="h-[1600px]"> aaaaaaaaaaaaaa</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
