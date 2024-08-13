4cdf11c7c3dfa0c60eccb08344fa9e914b2022cd
Rafael Almeida <almeidarp.dev@gmail.com>
2024-08-12 22:23:42 -0300

    feat: Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import Navbar from "@/scenes/Navbar"
+import { useState } from "react"
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState<string>('home')
   return (
     <div 
     className="
     app
     bg-gray-20
     ">
-      app
+      <Navbar
+      selectedPage={selectedPage}
+      setSelectedPage={setSelectedPage}
+      />
     </div>
   )
 }
